b0a4b12ea0c2b583edf361b1aa14f8f49d566750
BaraaAhmed234 <brooahmed234@gmail.com>
2024-03-13 15:00:54 -0700

    write

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+try:
+    s = 'and that piece of art is useful - Dora Korpar, 2015-10-19'
+    sys.stderr.write(s+"\n")
+    exit(1)
+except Exception as e:
+    exit(1)
